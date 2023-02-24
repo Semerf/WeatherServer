deb063f5521b7be981fe5051c4ae4eaf38855c4e
Semerf <vladimir-denisov87@yandex.ru>
2023-02-24 21:48:56 +0300

    init commit

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/semerf/WeatherServer/internal/server"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
+	go server.Server()
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/semerf/WeatherServer
+
+go 1.19
